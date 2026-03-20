9cfc21d626a2209c56770cb9eb74771aac392574
mahmoudatef0511 <mahmoudatef0511@gmail.com>
2026-03-20 07:31:33 +0200

    polishing code

--- index.php
+++ index.php
@@ -2,43 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// use Dotenv\Dotenv;
-
-// $dotenv = Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
-    exit();
-}
-
-// TEMPORARY DEBUG - remove after fixing
-if (isset($_GET['debug'])) {
-    $host     = getenv('MYSQLHOST');
-    $dbname   = getenv('MYSQLDATABASE');
-    $username = getenv('MYSQLUSER');
-    $password = getenv('MYSQLPASSWORD');
-    $port     = getenv('MYSQLPORT');
-
-    echo json_encode([
-        'host'     => $host,
-        'dbname'   => $dbname,
-        'username' => $username,
-        'port'     => $port,
-        'password' => $password ? '***set***' : 'NOT SET',
-    ]);
-
-    try {
-        $dsn = "mysql:host={$host};dbname={$dbname};port={$port};charset=utf8mb4";
-        $pdo = new PDO($dsn, $username, $password);
-        echo json_encode(['db' => 'CONNECTION SUCCESS']);
-    } catch (Exception $e) {
-        echo json_encode(['db' => 'FAILED: ' . $e->getMessage()]);
-    }
     exit();
 }
 
