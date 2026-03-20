daa34cefe9f03ada5e69fb984c341a0c1c70422e
mahmoudatef0511 <mahmoudatef0511@gmail.com>
2026-03-20 06:58:12 +0200

    debugging db

--- index.php
+++ index.php
@@ -16,7 +16,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+// TEMPORARY DEBUG - remove after fixing
+if (isset($_GET['debug'])) {
+    $host     = getenv('MYSQLHOST');
+    $dbname   = getenv('MYSQLDATABASE');
+    $username = getenv('MYSQLUSER');
+    $password = getenv('MYSQLPASSWORD');
+    $port     = getenv('MYSQLPORT');
+
+    echo json_encode([
+        'host'     => $host,
+        'dbname'   => $dbname,
+        'username' => $username,
+        'port'     => $port,
+        'password' => $password ? '***set***' : 'NOT SET',
+    ]);
+
+    try {
+        $dsn = "mysql:host={$host};dbname={$dbname};port={$port};charset=utf8mb4";
+        $pdo = new PDO($dsn, $username, $password);
+        echo json_encode(['db' => 'CONNECTION SUCCESS']);
+    } catch (Exception $e) {
+        echo json_encode(['db' => 'FAILED: ' . $e->getMessage()]);
+    }
+    exit();
+}
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
 });
 
