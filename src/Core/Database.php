d1bdf2c1ff8c157b5255e9187464c79593827c4e
mahmoudatef0511 <mahmoudatef0511@gmail.com>
2026-03-20 05:47:53 +0200

    DB config

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -23,12 +23,13 @@ class Database
 
     private static function createConnection(): PDO
     {
-        $host = $_ENV['DB_HOST'];
-        $dbname = $_ENV['DB_NAME'];
-        $username = $_ENV['DB_USER'];
-        $password = $_ENV['DB_PASS'];
+        $host = "mysql.railway.internal";
+        $dbname = "railway";
+        $username = "root";
+        $password = "BHfrufzkllYecpesgVYtYsxOVLwGrUIF";
+        $port = 3306;
 
-        $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+        $dsn = "mysql:host={$host};dbname={$dbname};port=$port;charset=utf8mb4";
 
         try {
             $pdo = new PDO($dsn, $username, $password, [
